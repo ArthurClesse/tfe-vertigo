3c336a13c52d451ad0c7a4a2b09f239773aece93
Arthur Clesse <61355022+ArthurClesse@users.noreply.github.com>
2021-03-23 11:06:49 +0100

    Update tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,66 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      spacing: {
-        enorme : "500px"
-      }
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1440px',
+      },
+      colors: {
+        transparent: 'transparent',
+      },
+      fontFamily: {
+        'sans' : ['Helvetica Neue', 'Arial', 'sans_serif'],
+        'serif' : ['Georgia','Times','Times New Roman','serif']
+      },
+      fontSize: {
+        'tiny' : '1.2rem',
+        'base' : '1.8rem',
+        'lg': '2.2rem',
+        'xl': '3.4rem',
+        '2xl': '5.6rem',
+        '3xl': '9.2rem',
+        '4xl': '17rem',
+      },
+      // Aspect ratio
+      aspectRatio: {
+        '16/9': [16, 9],
+        '4/3': [4, 3],
+        '21/9': [21, 9],
+        '1/1': [1, 1],
+        '3/4': [3, 4]
+      },
     },
   },
-  variants: {
-    extend: {},
+  extend: {
+    height: {
+      '1px': '1px',
+      '2px': '2px',
+      '4px': '4px',
+      '6px': '6px',
+      '8px': '8px',
+    },
+    width: {
+      '1px': '1px',
+      '2px': '2px',
+      '4px': '4px',
+      '6px': '6px',
+      '8px': '8px',
+    },
+    maxWidth: {
+      '7xl':'88rem',
+      'screen-sm':'64rem',
+      'screen-md':'76.8rem',
+      'screen-lg':'102.4rem',
+      'screen-xl':'128rem'
+    },
+    translate: {
+      'center': ['-50%', '-50%']
+    },
   },
-  plugins: [],
+  variants: {},
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
