21ceef662d526f4bbc80bf95f636a603c720b73b
Arthur Clesse <61355022+ArthurClesse@users.noreply.github.com>
2021-03-24 17:17:59 +0100

    animation balls + text

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,3 +9,23 @@ function toggleNavigation(){
     }
 }
 
+
+function randomTween(element){
+    TweenMax.to(element, {
+        x: Math.random() * 700,
+        y: Math.random() * 800,
+        duration: 3,
+        ease: Linear.easeNone,
+        onComplete:randomTween,
+        onCompleteParams: [element]
+    })
+  }
+  
+randomTween(document.querySelector(".circle-1"));
+randomTween(document.querySelector(".circle-2"));
+randomTween(document.querySelector(".circle-3"));
+randomTween(document.querySelector(".circle-4"));
+randomTween(document.querySelector(".circle-5"));
+randomTween(document.querySelector(".circle-6"));
+randomTween(document.querySelector(".circle-7"));
+randomTween(document.querySelector(".circle-8"));
