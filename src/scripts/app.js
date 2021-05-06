98b0abede3589b5fe9919f482427e29a65e82c21
Arthur Clesse <61355022+ArthurClesse@users.noreply.github.com>
2021-05-06 12:27:05 +0200

    page transition test

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,5 @@
+"use strict";
+//MENU
 let menuButton = document.querySelector(".btn-menu");
 menuButton.addEventListener("click", toggleNavigation);
 
@@ -30,6 +32,7 @@ randomTween(document.querySelector(".circle-6"));
 randomTween(document.querySelector(".circle-7"));
 randomTween(document.querySelector(".circle-8"));
 
+//Swipper JS
 function updateTextInput(val) {
     document.getElementById('textInput').innerHTML=val; 
   }
@@ -51,6 +54,7 @@ function updateTextInput(val) {
     },
   });
 
+
   function myFunction() {
     var x = document.getElementById("myText").value;
     let listElements = document.querySelectorAll(".vertigo");
@@ -60,3 +64,46 @@ function updateTextInput(val) {
         listElement.style.color = "red";
     }
   }
+
+  //Page transition
+const wipe = document.querySelector('.page-transition');
+const TLAnim = gsap.timeline();
+
+function delay(n) {
+    n = n || 2000;
+    return new Promise((done) => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
+
+barba.init({
+
+  sync: true,
+
+  transitions: [
+    {
+      async leave(data){
+
+        const done = this.async();
+
+        TLAnim.to(wipe, {left: '0%', ease: "power2.out", duration: 0.5});
+
+        await delay(500);
+        done();
+
+      },
+      enter(data){
+
+        TLAnim
+        .to(wipe, {left: '100%', ease:"power2.in", duration: 0.5})
+        .set(wipe, {left: '-110%'})
+        document.body.removeAttribute("data-menu");
+
+
+      }
+    }
+  ]
+
+})
