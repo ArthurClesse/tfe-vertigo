01bd9c6f83381316f4bc85d9aa0585f45a057ee1
Arthur Clesse <61355022+ArthurClesse@users.noreply.github.com>
2021-05-07 11:41:47 +0200

    Advance beta

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -14,8 +14,8 @@ function toggleNavigation(){
 
 function randomTween(element){
     TweenMax.to(element, {
-        x: Math.random() * 1500,
-        y: Math.random() * 2000,
+        x: Math.random() * 850,
+        y: Math.random() * 700,
         duration: 3,
         ease: Linear.easeNone,
         onComplete:randomTween,
@@ -31,6 +31,52 @@ randomTween(document.querySelector(".circle-5"));
 randomTween(document.querySelector(".circle-6"));
 randomTween(document.querySelector(".circle-7"));
 randomTween(document.querySelector(".circle-8"));
+
+//Color change cicle 1
+function circleColor(newColor) {
+  let textColor = document.querySelector(".text-color");
+  let listCircles = document.querySelectorAll(".circle-color1");
+
+  for(let listCircle of listCircles){
+    listCircle.style.backgroundColor = newColor;
+  }
+  textColor.innerHTML = newColor;
+}
+
+//Color change cicle 2
+function circleColor2(newColor) {
+  let textColor = document.querySelector(".text-color");
+  let list2Circles = document.querySelectorAll(".circle-color2");
+
+  for(let list2Circle of list2Circles){
+    list2Circle.style.backgroundColor = newColor;
+  }
+  textColor.innerHTML = newColor;
+}   
+
+//Slider range cirlce 1
+let slider = document.getElementById('range-circle1');
+let listCircles = document.querySelectorAll(".circle-color1");
+
+slider.addEventListener('input', e => {
+  for(let listCircle of listCircles){
+    listCircle.style.width = e.target.value + 'px';
+    listCircle.style.height = e.target.value + 'px'
+  }
+})
+
+
+//Slider range cirlce 2
+let slider2 = document.getElementById('range-circle2');
+let list2Circles = document.querySelectorAll(".circle-color2");
+
+slider2.addEventListener('input', e => {
+  for(let list2Circle of list2Circles){
+    list2Circle.style.width = e.target.value + 'px';
+    list2Circle.style.height = e.target.value + 'px'
+  }
+})
+
 
 //Swipper JS
 function updateTextInput(val) {
@@ -55,6 +101,7 @@ function updateTextInput(val) {
   });
 
 //Bêta Vertigo
+//Change text
   function changeText() {
     var x = document.getElementById("myText").value;
     let listElements = document.querySelectorAll(".vertigo");
@@ -63,7 +110,9 @@ function updateTextInput(val) {
         listElement.innerHTML = x;
     }
   }
-  function changeColor(newColor) { // definition of function
+
+//Change text color
+  function changeColor(newColor) {
     let textColor = document.querySelector(".text-color");
     let listElements = document.querySelectorAll(".vertigo");
 
@@ -72,10 +121,39 @@ function updateTextInput(val) {
     }
     textColor.innerHTML = newColor;
 }   
-function changeBackground(newBackground) { // definition of function
+
+//Change background color
+function changeBackground(newBackground) {
+    let bgColor = document.querySelector(".background-color");
     let bgChange = document.querySelector(".bg-change");
+
     bgChange.style.backgroundColor = newBackground;
-}   
+    bgColor.innerHTML = newBackground;
+} 
+
+//Without stroke text
+function noStroke(){
+    let listStrokes = document.querySelectorAll(".text-beta2");
+    for(let listStroke of listStrokes){
+        if(listStroke.classList.contains("stroke")){
+            listStroke.classList.remove("stroke");
+        }
+    }
+}
+
+//With stroke text
+function withStroke(){
+
+    let listStrokes = document.querySelectorAll(".text-beta2");
+    for(let listStroke of listStrokes){
+        if(listStroke.classList.contains("stroke")){
+        }
+        else{
+            listStroke.classList.add("stroke");
+        }
+    }
+}
+
   //Page transition
 // const wipe = document.querySelector('.page-transition');
 // const TLAnim = gsap.timeline();
