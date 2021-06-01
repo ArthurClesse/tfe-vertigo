ee6993d3940c8f2a8ea89fc1d58a11abc19af073
Arthur Clesse <61355022+ArthurClesse@users.noreply.github.com>
2021-06-01 10:51:17 +0200

    beta menu

--- src/scripts/beta.js
+++ src/scripts/beta.js
@@ -1,68 +1,68 @@
 "use strict";
-function randomTween(element){
-    TweenMax.to(element, {
-        x: Math.random() * 850,
-        y: Math.random() * 700,
-        duration: 3,
-        ease: Linear.easeNone,
-        onComplete:randomTween,
-        onCompleteParams: [element]
-    })
-  }
+// function randomTween(element){
+//     TweenMax.to(element, {
+//         x: Math.random() * 850,
+//         y: Math.random() * 700,
+//         duration: 3,
+//         ease: Linear.easeNone,
+//         onComplete:randomTween,
+//         onCompleteParams: [element]
+//     })
+//   }
   
-randomTween(document.querySelector(".circle-1"));
-randomTween(document.querySelector(".circle-2"));
-randomTween(document.querySelector(".circle-3"));
-randomTween(document.querySelector(".circle-4"));
-randomTween(document.querySelector(".circle-5"));
-randomTween(document.querySelector(".circle-6"));
-randomTween(document.querySelector(".circle-7"));
-randomTween(document.querySelector(".circle-8"));
+// randomTween(document.querySelector(".circle-1"));
+// randomTween(document.querySelector(".circle-2"));
+// randomTween(document.querySelector(".circle-3"));
+// randomTween(document.querySelector(".circle-4"));
+// randomTween(document.querySelector(".circle-5"));
+// randomTween(document.querySelector(".circle-6"));
+// randomTween(document.querySelector(".circle-7"));
+// randomTween(document.querySelector(".circle-8"));
 
-//Color change cicle 1
-function circleColor(newColor) {
-  let circleColor01 = document.querySelector(".text-color01");
-  let listCircles = document.querySelectorAll(".circle-color1");
+// //Color change cicle 1
+// function circleColor(newColor) {
+//   let circleColor01 = document.querySelector(".text-color01");
+//   let listCircles = document.querySelectorAll(".circle-color1");
 
-  for(let listCircle of listCircles){
-    listCircle.style.backgroundColor = newColor;
-  }
-  circleColor01.innerHTML = newColor;
-}
+//   for(let listCircle of listCircles){
+//     listCircle.style.backgroundColor = newColor;
+//   }
+//   circleColor01.innerHTML = newColor;
+// }
 
-//Color change cicle 2
-function circleColor2(newColor) {
-  let circleColor02 = document.querySelector(".text-color02");
-  let list2Circles = document.querySelectorAll(".circle-color2");
+// //Color change cicle 2
+// function circleColor2(newColor) {
+//   let circleColor02 = document.querySelector(".text-color02");
+//   let list2Circles = document.querySelectorAll(".circle-color2");
 
-  for(let list2Circle of list2Circles){
-    list2Circle.style.backgroundColor = newColor;
-  }
-  circleColor02.innerHTML = newColor;
-}   
+//   for(let list2Circle of list2Circles){
+//     list2Circle.style.backgroundColor = newColor;
+//   }
+//   circleColor02.innerHTML = newColor;
+// }   
 
-//Slider range cirlce 1
-let slider = document.getElementById('range-circle1');
-let listCircles = document.querySelectorAll(".circle-color1");
+// //Slider range cirlce 1
+// let slider = document.getElementById('range-circle1');
+// let listCircles = document.querySelectorAll(".circle-color1");
 
-slider.addEventListener('input', e => {
-  for(let listCircle of listCircles){
-    listCircle.style.width = e.target.value + 'px';
-    listCircle.style.height = e.target.value + 'px'
-  }
-})
+// slider.addEventListener('input', e => {
+//   for(let listCircle of listCircles){
+//     listCircle.style.width = e.target.value + 'px';
+//     listCircle.style.height = e.target.value + 'px'
+//   }
+// })
 
 
-//Slider range cirlce 2
-let slider2 = document.getElementById('range-circle2');
-let list2Circles = document.querySelectorAll(".circle-color2");
+// //Slider range cirlce 2
+// let slider2 = document.getElementById('range-circle2');
+// let list2Circles = document.querySelectorAll(".circle-color2");
 
-slider2.addEventListener('input', e => {
-  for(let list2Circle of list2Circles){
-    list2Circle.style.width = e.target.value + 'px';
-    list2Circle.style.height = e.target.value + 'px'
-  }
-})
+// slider2.addEventListener('input', e => {
+//   for(let list2Circle of list2Circles){
+//     list2Circle.style.width = e.target.value + 'px';
+//     list2Circle.style.height = e.target.value + 'px'
+//   }
+// })
 
 //Bêta Vertigo
 //Change text
@@ -117,3 +117,22 @@ function withStroke(){
         }
     }
 }
+
+const linkFace = document.querySelector(".link-facial");
+const linkGene = document.querySelector(".link-general");
+const containerFace = document.querySelector(".container-facial");
+const containerGene = document.querySelector(".container-general");
+
+
+linkFace.addEventListener("click", function(){
+  containerGene.classList.add("hidden");
+  linkGene.classList.remove("text-purple");
+  containerFace.classList.remove("hidden");
+  linkFace.classList.add("text-purple");
+});
+linkGene.addEventListener("click", function(){
+  containerGene.classList.remove("hidden");
+  linkGene.classList.add("text-purple");
+  containerFace.classList.add("hidden");
+  linkFace.classList.remove("text-purple");
+})
