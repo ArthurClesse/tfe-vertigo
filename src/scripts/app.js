4abf779825abff0ba3b39e6bc20f001145ec884a
Arthur Clesse <61355022+ArthurClesse@users.noreply.github.com>
2021-06-10 15:07:56 +0200

    last update

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -125,18 +125,3 @@ document.querySelectorAll('.reveal').forEach(function (r){
   observer.observe(r)
 })
 
-var webcam = document.querySelector('.webcam');
-var msgSafari = document.querySelector('.msg-safari');
-var textWidth = document.querySelector('.text-width');
-var ua = navigator.userAgent.toLowerCase(); 
-if (ua.indexOf('safari') != -1) { 
-  if (ua.indexOf('chrome') > -1) {
-    console.log("Tu es sur chrome")
-  } else {
-    webcam.classList.add('hide-safari');
-    textWidth.classList.add('hide-safari');
-    msgSafari.classList.add('show-safari');
-  }
-}
-
-
--- src/scripts/face-api.js
+++ src/scripts/face-api.js
@@ -37,3 +37,19 @@ video.addEventListener('play', () => {
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
   }, 100);
 });
+
+
+
+var webcam = document.querySelector('.webcam');
+var msgSafari = document.querySelector('.msg-safari');
+var textWidth = document.querySelector('.text-width');
+var ua = navigator.userAgent.toLowerCase(); 
+if (ua.indexOf('safari') != -1) { 
+  if (ua.indexOf('chrome') > -1) {
+    console.log("Tu es sur chrome")
+  } else {
+    webcam.classList.add('hide-safari');
+    textWidth.classList.add('hide-safari');
+    msgSafari.classList.add('show-safari');
+  }
+}
